9b8c831c14cd7d5947feb4198e41f8661510885c
EC2 Default User <ec2-user@ip-172-31-18-173.ec2.internal>
2021-08-18 11:53:46 +0000

    Ensure possible sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_customers_path
+    when Customer
+      public_root_path
+    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    if resource_class == Customer
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :given_name, :family_name_kana, :given_name_kana, :email, :postal_code, :address, :phone])
+      devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+    end
+  end
+
 
 end
