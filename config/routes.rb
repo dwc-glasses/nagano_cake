d7297b96d1a746f6ab345767013b94ae2dfaf6bc
Zakiwo <ec2-user@ip-172-31-91-244.ec2.internal>
2021-08-18 00:40:16 +0000

    fix root to

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     }
 
   namespace :admin do
-    root to   :'homes#top'
+    root to:'homes#top'
     resources :customers,      only: [:index, :show, :edit, :update]
     resources :products,       except: [:destroy]
     resources :genres,         only: [:index, :edit, :create, :update]
