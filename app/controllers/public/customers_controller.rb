7fb1b162eee99d3be3052a1f0a0292140c5a5470
EC2 Default User <ec2-user@ip-172-31-18-173.ec2.internal>
2021-08-19 04:45:17 +0000

    Add edit_page, suspend_page

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,32 +2,32 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def edit
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def update
     customer = Customer.find(current_customer.id)
     if customer.update(customer_params)
-      redirect_to :public_customers_path
+      flash[:notice] = "会員情報を更新しました"
+      redirect_to action: :show
     else
-      @customer = Customer.find(current_customer.id)
       render :edit
     end
   end
-  
+
   def suspend
   end
-  
+
   def suspended
     customer = Customer.find(current_customer.id)
     customer.update({suspended: true})
-    redirect_to :public_root_path
+    redirect_to action: :show
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:family_name, :given_name, :family_name_kana, :given_name_kana, :postal_code, :address, :phone, :email)
   end
