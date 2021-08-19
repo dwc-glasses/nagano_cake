f660fa6e5d3bd5b2a977805cb9b94979f61cd651
Zakiwo <ec2-user@ip-172-31-91-244.ec2.internal>
2021-08-19 09:23:30 +0000

    make show view and assosiation

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,6 @@
 class OrderProduct < ApplicationRecord
   belongs_to :product
+  belongs_to :order_info, foreign_key: "order_id"
+
   validates :price, :quantity, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_08_19_071355) do
     t.string "name"
   end
 
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,14 +59,6 @@ ActiveRecord::Schema.define(version: 2021_08_19_071355) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_products", force: :cascade do |t|
-    t.integer "order_id"
-    t.integer "product_id"
-    t.integer "price"
-    t.integer "quantity"
-    t.integer "product_status"
-  end
-    
   create_table "order_infos", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "postage", null: false
@@ -77,6 +68,16 @@ ActiveRecord::Schema.define(version: 2021_08_19_071355) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "price"
+    t.integer "quantity"
+    t.integer "product_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
