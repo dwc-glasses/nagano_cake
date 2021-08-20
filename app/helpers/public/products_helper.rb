feb0661d4fa1bc90b8d204ff19735357e2df0ccf
besumero628 <besumero628@gmail.com>
2021-08-20 12:10:47 +0000

    update product helper method

--- app/helpers/public/products_helper.rb
+++ app/helpers/public/products_helper.rb
@@ -1,2 +1,19 @@
 module Public::ProductsHelper
+  def product_form_hash
+    hash = { :image => ["商品イメージ", :attachment],
+                        :name => ["商品名", :field],
+                        :introduction => ["商品説明", :area],
+                        :genre_id => ["ジャンル", :genre],
+                        :price => ["価格（税抜き）", :field]  }
+    return hash
+  end
+  
+  def genre_form_array
+    array = Array.new
+      Genre.all.each do |g|
+        array << [g.name, g.id]
+      end 
+    return array
+  end
+  
 end
