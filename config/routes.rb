be88760637a0030a4ba48fb0df056fb401ebef1c
Zakiwo <ec2-user@ip-172-31-91-244.ec2.internal>
2021-08-21 02:38:04 +0000

    fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,9 @@ Rails.application.routes.draw do
     resources :customers,      only: [:index, :show, :edit, :update]
     resources :products,       except: [:destroy]
     resources :genres,         only: [:index, :edit, :create, :update]
-    resources :order_infos,    only: [:index, :show, :update]
-    resources :order_products, only: [:update]
+    resources :order_infos,    only: [:index, :show, :update] do
+     resources :order_products, only: [:update]
+    end
   end
 
   namespace :public, path:"" do
@@ -45,11 +46,13 @@ Rails.application.routes.draw do
 
     #cart_producst
     delete '/cart_products/delete_all' => 'cart_products#delete_all'
-    resources :order_infos,  only:[:index, :show, :new, :create]
+    
 
     #order_infos
     get '/order_infos/confirm'         => 'order_infos#confirm'
     get '/order_infos/complete'        => 'order_infos#complete'
+    resources :order_infos,  only:[:index, :show, :new, :create]
+
   end
 
 end
