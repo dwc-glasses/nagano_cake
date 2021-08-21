b2e10b1ffeb3a414d1c872e8c1c11719b831b266
EC2 Default User <ec2-user@ip-172-31-18-173.ec2.internal>
2021-08-21 09:51:24 +0000

    Add function genre search

---
+++ app/controllers/public/searchs_controller.rb
@@ -0,0 +1,43 @@
+class Public::SearchsController < Public::Base
+  def search
+    @value = params["search"]["value"]
+    @how = params["search"]["how"]
+    @datas = search_for(@value, @how)
+    @searched_value = unless @value.to_i == 0
+      Genre.find(@value.to_i).name
+    else
+      @value
+    end
+  end
+
+  private
+  def match(value)
+    Product.where(genre_id: value).or(Product.where(name: value))
+  end
+
+  def forward(value)
+    Genre.where("name LIKE ?", "#{value}%").or(Product.where("name LIKE ?", "#{value}%"))
+  end
+
+  def backward(value)
+    Genre.where("name LIKE ?", "%#{value}").or(Product.where("name LIKE ?", "%#{value}"))
+  end
+
+  def partical(value)
+    Genre.where("name LIKE ?", "%#{value}%").or(Product.where("name LIKE ?", "%#{value}%"))
+  end
+
+  def search_for(value, how)
+    case how
+    when 'match'
+      match(value)
+    when 'forward'
+      forwad(value)
+    when 'backward'
+      backward(value)
+    when 'partical'
+      partical(value)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'searchs/search'
+  end
+  get 'searchs/search'
   #namespace内ではエラーが出たため外に記述
   devise_for :customers, skip: 'registrations', controllers: {
       sessions:      'customers/sessions',
@@ -47,11 +51,13 @@ Rails.application.routes.draw do
     #cart_producst
     get '/cart_products/delete_all' => 'cart_products#delete_all'
     
-
     #order_infos
     get '/order_infos/confirm'         => 'order_infos#confirm'
     get '/order_infos/complete'        => 'order_infos#complete'
     resources :order_infos,  only:[:index, :show, :new, :create]
+    
+    #searchs
+    get '/search' => 'searchs#search'
 
   end
 
