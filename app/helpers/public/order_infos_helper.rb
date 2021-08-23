65896c725106ea749ce797e5eae98c1d057fb879
Zakiwo <ec2-user@ip-172-31-91-244.ec2.internal>
2021-08-23 06:08:46 +0000

    move status to helper

--- app/helpers/public/order_infos_helper.rb
+++ app/helpers/public/order_infos_helper.rb
@@ -1,2 +1,11 @@
 module Public::OrderInfosHelper
+  def order_status_hash
+    order_hash = { '0': "入金待ち", '1': "入金確認", '2':"製作中", '3':"発送準備中", '4':"発送済み" }
+    return order_hash
+  end
+  
+  def payment_method_hash
+    payment_hash = { '0': "クレジットカード", '1': "銀行振込" }
+    return payment_hash
+  end
 end
